bd37411a0527cc80aa1ff32a5c3c9c71edc768d1
Wisdom Okwen <wisdomokwen@gmail.com>
2025-07-06 00:09:45 -0700

    Tabularize skills

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Footer: React.FC = () => (
+  <footer style={{ textAlign: 'center', padding: '1rem 0', fontSize: '1rem', color: '#888' }}>
+    Made with <span style={{ color: '#0074D9', fontWeight: 'bold' }}>&hearts;</span> by Wisdom Okwen
+  </footer>
+);
+
+export default Footer;
