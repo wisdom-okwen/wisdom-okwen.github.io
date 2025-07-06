25d24cb0e90c02043dbe56875d78da235af329c4
Wisdom Okwen <wisdomokwen@gmail.com>
2025-07-05 17:52:49 -0700

    Styling

--- src/components/Education.tsx
+++ src/components/Education.tsx
@@ -5,8 +5,15 @@ const Education: React.FC = () => {
     <section id="education">
       <h2>👨‍🎓 Education</h2>
       <hr style={{ width: '60%', border: '0.1px solid #cccdceff', margin: '0.5rem 0 1rem 0' }} />
-      <ul>
-        <li>University of North Carolina at Chapel Hill - Bachelor of Science in Computer Science, May 2026</li>
+      <ul style={{ listStyle: 'none', padding: 0 }}>
+        <li>
+          <strong>Spring ‘25:</strong> University of North Carolina at Chapel Hill
+          <ul style={{ listStyle: 'none', paddingLeft: '5em', margin: 0 }}>
+            <li>B.S. Computer Science</li>
+            <li>Distinction, Phi Beta Kappa</li>
+            <li>GPA: 3.6</li>
+          </ul>
+        </li>
       </ul>
     </section>
   );
