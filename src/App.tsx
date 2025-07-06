19f3e2ad5352f65931815ea9f6731327f7196363
Wisdom Okwen <wisdomokwen@gmail.com>
2025-07-05 19:01:33 -0700

    Rename repo

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 
-import React from 'react';
+
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Homepage from './components/Homepage';
