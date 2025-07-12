8ced7e841be3127881733b31a5d211bf74f2250a
Wisdom Okwen <wisdomokwen@gmail.com>
2025-07-11 19:20:27 -0700

    dd padding in phone screen mode

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer: React.FC = () => (
   <footer style={{ textAlign: 'center', padding: '1rem 0', fontSize: '1rem', color: '#888' }}>
-    Made with <span style={{ color: '#0074D9', fontWeight: 'bold' }}>&hearts;</span> by Wisdom Okwen
+    Made with <span >💙</span> by Wisdom Okwen
   </footer>
 );
 
