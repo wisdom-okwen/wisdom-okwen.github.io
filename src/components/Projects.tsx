1d2d8ec2e7bc59ba39902635aaa79b7250858c62
Wisdom Okwen <wisdomokwen@gmail.com>
2025-07-26 15:42:21 -0700

    Remove projects

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -6,9 +6,10 @@ const Projects: React.FC = () => {
       <h2>👨‍💻 Projects</h2>
       <hr style={{ width: '60%', border: '0.1px solid #cccdceff', margin: '0.5rem 0 1rem 0' }} />
       <ul>
-        <li>2024: <a href="https://github.com/wisdom-okwen/ml-california-housing" target="_blank" rel="noopener noreferrer">California Housing Price Predictor</a></li>
+        <li><a href="https://github.com/wisdom-okwen" target="_blank" rel="noopener noreferrer">Check GitHub</a></li>
+        {/* <li>2024: <a href="https://github.com/wisdom-okwen/ml-california-housing" target="_blank" rel="noopener noreferrer">California Housing Price Predictor</a></li>
         <li>2024: <a href="https://csxl.unc.edu/welcome" target="_blank" rel="noopener noreferrer">UNC CSXL Website</a></li>
-        <li>2023: <a href="https://github.com/wisdom-okwen/comp-560-project" target="_blank" rel="noopener noreferrer">Bank Customer Churn Prediction</a></li>
+        <li>2023: <a href="https://github.com/wisdom-okwen/comp-560-project" target="_blank" rel="noopener noreferrer">Bank Customer Churn Prediction</a></li> */}
       </ul>
     </section>
   );
